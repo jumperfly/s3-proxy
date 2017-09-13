256ad7f5f8e46fc186c9ccfeec2a571a32e66f04
jumperfly <git@jumperfly.co.uk>
2017-09-13 18:30:29 +0100

    Updated to support subdirs.

--- src/main/java/uk/co/jumperfly/aws/s3proxy/controller/S3ProxyController.java
+++ src/main/java/uk/co/jumperfly/aws/s3proxy/controller/S3ProxyController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.InputStream;
@@ -19,8 +22,10 @@ public class S3ProxyController {
     @Autowired
     private AmazonS3 s3Client;
 
-    @RequestMapping("/s3/{bucket}/{key:.+}")
-    public StreamingResponseBody get(@PathVariable String bucket, @PathVariable String key) {
+    @RequestMapping("/s3/{bucket}/**")
+    public StreamingResponseBody get(@PathVariable String bucket, WebRequest request) {
+        String key = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE,
+                RequestAttributes.SCOPE_REQUEST).toString().substring(bucket.length() + 5);
         S3Object object = s3Client.getObject(new GetObjectRequest(bucket, key));
 
         return out -> {
